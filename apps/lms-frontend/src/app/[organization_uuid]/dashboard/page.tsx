284c5582a8d0be0fc981da605c6840c9001db623
vasu.dev <vasudev.1160@zenmonk.tech>
2026-06-12 11:41:44 +0530

    feature:updated docker files for better development experience.

--- apps/lms-frontend/src/app/[organization_uuid]/dashboard/page.tsx
+++ apps/lms-frontend/src/app/[organization_uuid]/dashboard/page.tsx
@@ -12,6 +12,7 @@ const UserDashBoard = async ({ params }: PageProps) => {
   const { organization_uuid } = await params;
   return (
     <div className="p-4 w-11/12 min-[1400px]:w-3/4 mx-auto">
+      <h1> this is vasudev </h1>
     <Dashboard
       organization_uuid={organization_uuid}
       email={session?.user?.email ?? ""}
