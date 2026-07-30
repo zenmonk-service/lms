5f820a47d8a5949063afc7a52cbac9d8b4f4f2ba
vasu.dev <vasudev.1160@zenmonk.tech>
2026-07-30 10:51:15 +0530

    feat: remove unused holiday management functions and streamline holiday filtering logic

--- apps/lms-backend/routes/holiday-route.js
+++ apps/lms-backend/routes/holiday-route.js
@@ -1,17 +1,6 @@
 const router = require("express").Router();
 const { holidayControllers } = require("../controllers");
-const { acl } = require("../middleware/acl-middleware");
-const { Action } = require("../models/common/action-enum");
-const { Permission } = require("../models/common/permission-enum");
 
-router
-  .route("/")
-  .get(acl(Permission.ENUM.HOLIDAY_MANAGEMENT, Action.ENUM.READ),holidayControllers.getFilteredHoliday)
-  .post(acl(Permission.ENUM.ATTENDANCE_MANAGEMENT, Action.ENUM.CREATE),holidayControllers.createHoliday);
-
-router
-  .route("/:holiday_uuid")
-  .get(acl(Permission.ENUM.HOLIDAY_MANAGEMENT, Action.ENUM.READ),holidayControllers.getHolidayById)
-  .put(acl(Permission.ENUM.HOLIDAY_MANAGEMENT, Action.ENUM.UPDATE),holidayControllers.updateHoliday);
+router.route("/").get(holidayControllers.getFilteredHoliday);
 
 module.exports = router;
