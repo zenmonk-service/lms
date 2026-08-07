3e39095eb5eb50b88ec3949c0fa621b0a62c25de
vasu.dev <vasudev.1160@zenmonk.tech>
2026-08-07 14:40:45 +0530

    feat: enhance leave type controller with improved error handling and add SLA to leave balance

--- apps/lms-backend/controllers/leave-type-controller.js
+++ apps/lms-backend/controllers/leave-type-controller.js
@@ -3,62 +3,68 @@ const { NotFoundError } = require("../middleware/error");
 const { leaveTypeService } = require("../services");
 
 exports.createLeaveType = async (req, res, next) => {
-    try {
-        await leaveTypeService.createLeaveType(req);
-        res.status(HTTP_STATUS_CODE.ENUM.CREATED).json({ message: "Leave type created successfully." });
-    } catch (err) {
-        console.log('err: ', err);
-        next(err);
-    }
+  try {
+    await leaveTypeService.createLeaveType(req);
+    res
+      .status(HTTP_STATUS_CODE.ENUM.CREATED)
+      .json({ message: "Leave type created successfully." });
+  } catch (err) {
+    console.log("err: ", err);
+    next(err);
+  }
 };
 
 exports.getFilteredLeaveTypes = async (req, res, next) => {
-    try {
-        const response = await leaveTypeService.getFilteredLeaveTypes(req);
-        res.status(HTTP_STATUS_CODE.ENUM.OK).json(response);
-    } catch (err) {
-        next(err);
-    }
+  try {
+    const response = await leaveTypeService.getFilteredLeaveTypes(req);
+    res.status(HTTP_STATUS_CODE.ENUM.OK).json(response);
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.getLeaveTypeById = async (req, res, next) => {
-    try {
-        const response = await leaveTypeService.getLeaveTypeById(req);
-        if (!response) throw new NotFoundError("Leave Type not found.");
-        res.status(HTTP_STATUS_CODE.ENUM.OK).json(response);
-    } catch (err) {
-        next(err);
-    }
+  try {
+    const response = await leaveTypeService.getLeaveTypeById(req);
+    if (!response) throw new NotFoundError("Leave Type not found.");
+    res.status(HTTP_STATUS_CODE.ENUM.OK).json(response);
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.updateLeaveTypeById = async (req, res, next) => {
-    try {
-        await leaveTypeService.updateLeaveTypeById(req);
-        res.status(HTTP_STATUS_CODE.ENUM.OK).json({ message: "Leave type updated successfully." });
-    } catch (err) {
-        next(err);
-    }
+  try {
+    await leaveTypeService.updateLeaveTypeById(req);
+    res
+      .status(HTTP_STATUS_CODE.ENUM.OK)
+      .json({ message: "Leave type updated successfully." });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.activateLeaveType = async (req, res, next) => {
   try {
-      await leaveTypeService.activateLeaveType(req);
-      res.status(HTTP_STATUS_CODE.ENUM.OK).json({ message: "Leave Type activated successfully." });
+    await leaveTypeService.activateLeaveType(req);
+    res
+      .status(HTTP_STATUS_CODE.ENUM.OK)
+      .json({ message: "Leave Type activated successfully." });
   } catch (err) {
-      next(err);
+    next(err);
   }
-}
+};
 
-exports.deactivateLeaveType= async (req, res, next) => {
+exports.deactivateLeaveType = async (req, res, next) => {
   try {
-      await leaveTypeService.deactivateLeaveType(req);
-      res.status(HTTP_STATUS_CODE.ENUM.OK).json({ message: "Leave Type deactivated successfully." });
+    await leaveTypeService.deactivateLeaveType(req);
+    res
+      .status(HTTP_STATUS_CODE.ENUM.OK)
+      .json({ message: "Leave Type deactivated successfully." });
   } catch (err) {
-      next(err);
+    next(err);
   }
-}
-
-
+};
 
 exports.getUserLeaveBalances = async (req, res, next) => {
   try {
@@ -67,4 +73,16 @@ exports.getUserLeaveBalances = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
+
+exports.addSlaToLeaveBalance = async (req, res, next) => {
+  try {
+    await leaveTypeService.addSlaToLeaveBalance(req);
+    res
+      .status(HTTP_STATUS_CODE.ENUM.CREATED)
+      .json({ message: "Leave Balance updated successfully." });
+  } catch (err) {
+    console.log("err: ", err);
+    next(err);
+  }
+};
