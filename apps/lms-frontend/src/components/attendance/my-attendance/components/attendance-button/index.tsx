57b1f78d961fc5fa76d98ea2f46ec06ae14244c9
Ankit Kumar Gupta <ankitkumar2003b@gmail.com>
2026-09-03 18:35:57 +0530

    feat: enhance AttendanceButton with real-time check-in capability and state management

--- apps/lms-frontend/src/components/attendance/my-attendance/components/attendance-button/index.tsx
+++ apps/lms-frontend/src/components/attendance/my-attendance/components/attendance-button/index.tsx
@@ -5,17 +5,34 @@ import { AttendanceConfirmDialog } from "../attendance-confirm-modal";
 import { Button } from "@/components/ui/button";
 import { LoaderCircle, Play, Square } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { PermissionAction, PermissionTag } from "@/features/permissions/permission.type";
+import {
+  PermissionAction,
+  PermissionTag,
+} from "@/features/permissions/permission.type";
 import { usePermissionCheck } from "@/hooks/use-permission-check";
+import { useAppSelector } from "@/store";
+import { useEffect, useState } from "react";
 
 interface Props {
   size?: "default" | "icon" | "icon-lg" | "icon-sm" | "lg" | "sm";
   className?: string;
 }
+const timeToSeconds = (time: string) => {
+  if (!time) return 0;
+  const [hours, minutes, seconds] = time.split(":").map(Number);
+
+  return hours * 3600 + minutes * 60 + seconds;
+};
 
 export function AttendanceButton({ size = "lg", className }: Props) {
   const can = usePermissionCheck();
-  const canUpdate = can(PermissionTag.USER_ATTENDANCE_MANAGEMENT, PermissionAction.UPDATE);
+  const orgSetting = useAppSelector(
+    (state) => state.organizationsSlice.roleSpecificOrganizationSettings,
+  );
+  const canUpdate = can(
+    PermissionTag.USER_ATTENDANCE_MANAGEMENT,
+    PermissionAction.UPDATE,
+  );
 
   const {
     isCheckedIn,
@@ -28,6 +45,25 @@ export function AttendanceButton({ size = "lg", className }: Props) {
     handleAttendanceClick,
     handleProcessAttendance,
   } = useAttendanceButton();
+
+  const [now, setNow] = useState(() => new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNow(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const currentSeconds =
+    now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+
+  const canCheckIn =
+    orgSetting?.end_time != null &&
+    currentSeconds <
+      timeToSeconds(orgSetting.end_time) +
+        Number(orgSetting?.flexible_time ?? 0) * 60;
 
   function getAttendanceButtonState(isCheckedIn: boolean) {
     return isCheckedIn
@@ -45,7 +81,7 @@ export function AttendanceButton({ size = "lg", className }: Props) {
 
   return (
     <>
-      {canUpdate && (
+      {canUpdate && (canCheckIn || isCheckedIn) && (
         <div className="flex flex-col">
           <Button
             variant={isCheckedIn ? "destructive" : "default"}
