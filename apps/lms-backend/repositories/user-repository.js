85339523ccb2a1c769d4443d9d6343e5977fa37e
Ankit Kumar Gupta <ankitkumar2003b@gmail.com>
2026-09-15 13:07:13 +0530

    feature: add affected_hours attribute to user attendance report

--- apps/lms-backend/repositories/user-repository.js
+++ apps/lms-backend/repositories/user-repository.js
@@ -357,7 +357,7 @@ class UserRepository extends BaseRepository {
         model: this.tenant(db.tenants.attendance),
         required: !!status,
         where: attendanceCriteria,
-        attributes: ["date", "status", "check_in", "check_out", "uuid"],
+        attributes: ["date", "status", "check_in", "check_out", "uuid" , "affected_hours"],
       });
     }
 
